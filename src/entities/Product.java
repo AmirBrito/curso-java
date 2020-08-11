8a08fc1b74750ffa555b949458434a81efaf4de8
amirsb <amir.sbrito@gmail.com>
2020-08-11 20:45:04 -0300

    add gets e sets na class product

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,6 +6,27 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public int getQuantity() {
+		return this.quantity;
+	}
+	
 	public double TotalValueInStock() {
 		return this.quantity * this.price;
 	}
