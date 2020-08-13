5064fb052dd55051507bacc39b4c49b4c3f27634
amirsb <amir.sbrito@gmail.com>
2020-08-13 00:13:55 -0300

    finalizando a class student e implementando na main

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Employee;
+import entities.Student;
+
+
 
 
 public class Main {
@@ -13,27 +15,34 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Employee emp = new Employee();
+		Student student = new Student();
 		
-		System.out.println("Name: ");
+		System.out.println("Informe os dados do estudante: ");
+		System.out.println("Nome: ");
 		String name = sc.nextLine();
-		emp.setName(name);
-		System.out.println("Gross Salary: ");
-		double grossSalary = sc.nextDouble();
-		emp.setGrossSalary(grossSalary);
-		System.out.println("Tax: ");
-		double tax = sc.nextDouble();
-		emp.setTax(tax);
+		System.out.println("Nota 1:");
+		int nota = sc.nextInt();
 		
-		System.out.println();
-		System.out.println("Employee: " + emp);
-		System.out.println();
-		System.out.println("Insert percentage to increase salary: ");
-		double percentage = sc.nextDouble();
-		emp.increaseSalary(percentage);
+		student.setName(name);
+		student.setNota1(nota);
 		
-		System.out.println();
-		System.out.println("Update data: " + emp);		
+		System.out.println("Nota 2:");
+		nota = sc.nextInt();
+		student.setNota2(nota);
+		
+		System.out.println("Nota 3:");
+		nota = sc.nextInt();
+		student.setNota3(nota);
+		
+		System.out.printf("Nota final = %.2f%n", student.notaFinal());
+		if(student.notaFinal() < 60) {
+			System.out.printf("Reprovado! Falta: %.2f%n", student.faltaPontos());
+		}
+		else {
+			System.out.println("Aprovado!");
+		}
+		
+			
 		sc.close();
 
 	}
