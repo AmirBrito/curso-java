c51e778a7f8ec9fcfebe0a71a94f9e0041e96f77
amirsb <amir.sbrito@gmail.com>
2020-08-14 00:17:06 -0300

    implementando construtor e sobrecarga na class product

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,6 +6,16 @@ public class Product {
 	private double price;
 	private int quantity;
 	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
 	
 	public void setName(String name) {
 		this.name = name;
